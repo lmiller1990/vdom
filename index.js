a661e732fac788d3ca008cd4749d273e517dd196
lachlan <lachlan.miller.1990@outlook.com>
2017-09-03 22:05:23 +0900

    Support properties

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /**
  * @jsx h
  */
+function flatten(arr) {
+	return [].concat.apply([], arr)
+}
+
+function h(type, props, ...children) {
+	props = props || {}
+	return { type, props, children } //: flatten(children) }
+}
+
 function view(count) {
 	return(
 		<ul id="cool" className="cool">
@@ -8,8 +17,40 @@ function view(count) {
 				Line 1
 			</li>
 			<li className="tester">
-				Line 1
+				Line 2
 			</li>
 		</ul>
 	)
+}
+
+function setProp(target, name, value) {
+	if (name === 'className') {
+		return target.setAttribute('class', value)
+	}
+	target.setAttribute(name, value)
+}
+
+function setProps(target, props) {
+	Object.keys(props).forEach(name => {
+		setProp(target, name, props[name])
+	})
+}
+
+function createElement(node) {
+	if (typeof node === 'string') {
+		return document.createTextNode(node) 
+	}
+	const el = document.createElement(node.type)
+
+	setProps(el, node.props) 
+
+ 	node.children
+		.map(createElement)
+		.forEach(el.appendChild.bind(el))
+
+	return el
+}
+
+function render(el) {
+	el.appendChild(createElement(view(0)))
 }
--- vdom.js
+++ vdom.js
@@ -1,6 +1,16 @@
 /**
  * @jsx h
  */
+function flatten(arr) {
+	return [].concat.apply([], arr);
+}
+
+function h(type, props, ...children) {
+	props = props || {};
+	return { type, props, children //: flatten(children) }
+	};
+}
+
 function view(count) {
 	return h(
 		"ul",
@@ -13,7 +23,37 @@ function view(count) {
 		h(
 			"li",
 			{ className: "tester" },
-			"Line 1"
+			"Line 2"
 		)
 	);
+}
+
+function setProp(target, name, value) {
+	if (name === 'className') {
+		return target.setAttribute('class', value);
+	}
+	target.setAttribute(name, value);
+}
+
+function setProps(target, props) {
+	Object.keys(props).forEach(name => {
+		setProp(target, name, props[name]);
+	});
+}
+
+function createElement(node) {
+	if (typeof node === 'string') {
+		return document.createTextNode(node);
+	}
+	const el = document.createElement(node.type);
+
+	setProps(el, node.props);
+
+	node.children.map(createElement).forEach(el.appendChild.bind(el));
+
+	return el;
+}
+
+function render(el) {
+	el.appendChild(createElement(view(0)));
 }
